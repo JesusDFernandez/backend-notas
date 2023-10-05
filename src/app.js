c802ae7bc28368150f758b1f5388db1b9fb56aa5
JesusDFernandez <78115281+JesusDFernandez@users.noreply.github.com>
2023-10-05 17:07:46 -0400

    agregando las routes y controllers

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-
+import authRoutes from "./routes/auth.routes.js";
+import taksRoutes from "./routes/tasks.routes.js";
 import { FRONTEND_URL } from "./config.js";
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
 
-
+app.use("/api/auth", authRoutes);
+app.use("/api", taksRoutes);
 
 export default app;
