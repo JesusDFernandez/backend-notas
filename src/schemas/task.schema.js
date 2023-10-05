7a3d1b8f85d95e3bd428bcc6ee5fccca394918af
JesusDFernandez <78115281+JesusDFernandez@users.noreply.github.com>
2023-10-05 15:21:39 -0400

    agregando los schema

---
+++ src/schemas/task.schema.js
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const createTaskSchema = z.object({
+  title: z.string({
+    required_error: "Se requiere título",
+  }),
+  description: z.string().optional()
+});
